0f8c4ee01299f2eaa11664957e699de3772a8090
chriswebb09 <christopher.webb@flatironschool.com>
2017-10-18 17:55:59 -0400

    Initial Commit

---
+++ ARNavigation/ARNavigation.h
@@ -0,0 +1,19 @@
+//
+//  ARNavigation.h
+//  ARNavigation
+//
+//  Created by Christopher Webb-Orenstein on 10/18/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARNavigation.
+FOUNDATION_EXPORT double ARNavigationVersionNumber;
+
+//! Project version string for ARNavigation.
+FOUNDATION_EXPORT const unsigned char ARNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARNavigation/PublicHeader.h>
+
+
